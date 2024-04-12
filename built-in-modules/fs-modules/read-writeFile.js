ec35abcf6326b44da09710a5e4cdf48990dfff29
GopiBalar <gopibalar1234@gmail.com>
2024-04-12 14:52:05 +0530

    readFile & writeFile

---
+++ built-in-modules/fs-modules/read-writeFile.js
@@ -0,0 +1,39 @@
+// const fs = require("fs");
+const fs = require("fs/promises");
+
+fs.readFile("./info.txt", "utf-8", (err, data) => {
+  if (err) {
+    console.log("Somthing wrong");
+  }
+  fs.writeFile("./info-backup.txt", data, (err) => {
+    if (err) {
+      console.log("Somthing wrong");
+    }
+    console.log("Successfully");
+  });
+});
+
+// promise  version
+fs.readFile("./info.txt", "utf-8")
+  .then((data) => {
+    fs.writeFile("./info-backup.txt", data, { flag: "a" }).then(() => {
+      console.log("succefuly");
+    });
+  })
+  .catch((err) => {
+    console.log("Somthing wrong");
+  });
+
+// async  
+async function backup() {
+  try {
+    const data = await fs.readFile("./info.txt", "utf-8");
+    await fs.writeFile("./info-backup.txt", data);
+    console.log("Successfully");
+    // fs.unlink('./info.txt');
+  } catch (err) {
+    console.log("somthing wrong");
+  }
+}
+
+backup();
